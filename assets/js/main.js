6e3a554de259ae8dee5b2ae69a4d9b5ce8a76eef
bluebamus <bluebamus@naver.com>
2022-02-08 15:42:58 +0900

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,59 @@
     });
     wow.init();
 
+    /*---canvas menu activation---*/
 
+    let canvasToggler = document.querySelector(".canvas_open"),
+     offcanvasMenuToggler = document.querySelector(".offcanvas_menu_wrapper"),
+     bodyOverlayToggler = document.querySelector(".body_overlay"),
+     canvasCloseToggler = document.querySelector(".canvas_close");
 
+    canvasToggler.addEventListener('click', function() {
+        navbarToggler.classList.toggle("active");
+        offcanvasMenuToggler.classList.toggle("active");
+    }) 
+
+    canvasCloseToggler.addEventListener('click', function() {
+        offcanvasMenuToggler.classList.toggle("active");
+        bodyOverlayToggler.classList.toggle("active");
+    }) 
+
+    bodyOverlayToggler.addEventListener('click', function() {
+        offcanvasMenuToggler.classList.toggle("active");
+        bodyOverlayToggler.classList.toggle("active");
+    }) 
+
+    /*--$('.canvas_open').on('click', function(){
+        $('.offcanvas_menu_wrapper,.body_overlay').addClass('active')
+    });
+    
+    $('.canvas_close,.body_overlay').on('click', function(){
+        $('.offcanvas_menu_wrapper,.body_overlay').removeClass('active')
+    });---*/
+
+    /*---Off Canvas Menu---*/
+    var $offcanvasNav = $('.offcanvas_main_menu'),
+        $offcanvasNavSubMenu = $offcanvasNav.find('.sub-menu');
+    $offcanvasNavSubMenu.parent().prepend('<span class="menu-expand"><i class="fa fa-angle-down"></i></span>');
+    
+    $offcanvasNavSubMenu.slideUp();
+    
+    $offcanvasNav.on('click', 'li a, li .menu-expand', function(e) {
+        var $this = $(this);
+        if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+            e.preventDefault();
+            if ($this.siblings('ul:visible').length){
+                $this.siblings('ul').slideUp('slow');
+            }else {
+                $this.closest('li').siblings('li').find('ul:visible').slideUp('slow');
+                $this.siblings('ul').slideDown('slow');
+            }
+        }
+        if( $this.is('a') || $this.is('span') || $this.attr('clas').match(/\b(menu-expand)\b/) ){
+        	$this.parent().toggleClass('menu-open');
+        }else if( $this.is('li') && $this.attr('class').match(/\b('menu-item-has-children')\b/) ){
+        	$this.toggleClass('menu-open');
+        }
+    });
 
 })();
